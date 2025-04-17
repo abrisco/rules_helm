651385d2957b59bfd2f3c981565f94f67b2b0737
abrisco <91817010+abrisco@users.noreply.github.com>
2025-04-17 22:38:26 +0000

    Deploying to gh-pages from @ abrisco/rules_helm@dbcf607cee47b98ec0362069a5f11b653561c4f2 🚀

--- toc.js
+++ toc.js
@@ -10,7 +10,7 @@ class MDBookSidebarScrollbox extends HTMLElement {
     connectedCallback() {
         this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded affix "><a href="index.html">Introduction</a></li><li class="chapter-item expanded affix "><li class="spacer"></li><li class="chapter-item expanded "><a href="rules.html"><strong aria-hidden="true">1.</strong> Rules</a></li></ol>';
         // Set the current, active page, and reveal it if it's hidden
-        let current_page = document.location.href.toString();
+        let current_page = document.location.href.toString().split("#")[0];
         if (current_page.endsWith("/")) {
             current_page += "index.html";
         }
